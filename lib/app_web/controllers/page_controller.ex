3351cc10eb543aa3fb05f74793d3871f394ef642
VS <v@s>
2019-11-26 19:30:48 +0300

    Layout & dummy output

--- lib/app_web/controllers/page_controller.ex
+++ lib/app_web/controllers/page_controller.ex
@@ -2,6 +2,56 @@ defmodule AppWeb.PageController do
   use AppWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    categories = [
+      %{
+        name: "Foo",
+        description: "FooDescription",
+        anchor: "foo",
+        libraries: [
+          %{
+            name: "fooOne",
+            description:
+              "Awesome Foo library for <a href=\"https://elixir-lang.org/\" target=_blank>Elixir</a>",
+            url: "https://github.com/foo/one/",
+            stars: 100,
+            updated_ago_days: 100,
+            outdated?: false
+          },
+          %{
+            name: "fooTwo",
+            description: "Foo library for Elixir №1 in the World",
+            url: "https://github.com/foo/two/",
+            stars: 200,
+            updated_ago_days: 222,
+            outdated?: true
+          }
+        ]
+      },
+      %{
+        name: "Bar",
+        description: "BarDescription",
+        anchor: "bar",
+        libraries: [
+          %{
+            name: "barLib",
+            description: "Simple and fast Bar package",
+            url: "https://github.com/bar/lib/",
+            stars: 70,
+            updated_ago_days: 400,
+            outdated?: true
+          },
+          %{
+            name: "justBar",
+            description: "Most rumored Bar lib for Phoenix",
+            url: "https://github.com/bar/another/",
+            stars: 99,
+            updated_ago_days: 70,
+            outdated?: false
+          }
+        ]
+      }
+    ]
+
+    render(conn, "index.html", %{categories: categories})
   end
 end
