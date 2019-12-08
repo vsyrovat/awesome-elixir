4c79edde663c10cd5dfde5007f8945aade084c69
VS <v@s>
2019-12-08 22:37:19 +0300

    Support for "min_stars" get params

--- lib/app_web/controllers/page_controller.ex
+++ lib/app_web/controllers/page_controller.ex
@@ -14,8 +14,29 @@ defmodule AppWeb.PageController do
     |> Map.merge(%{libraries: Enum.map(c.repositories, &transform_repository/1)})
   end
 
-  def index(conn, _params) do
-    categories = AwesomeProvider.categories() |> Enum.map(&transform_category/1)
+  defp filter_by_stars(categories, min_stars) when is_binary(min_stars) do
+    case Integer.parse(min_stars) do
+      {int, ""} -> filter_by_stars(categories, int)
+      _ -> categories
+    end
+  end
+
+  defp filter_by_stars(categories, min_stars) when is_integer(min_stars) and min_stars > 0 do
+    categories
+    |> Enum.map(fn c ->
+      %{c | libraries: Enum.filter(c.libraries, fn r -> r.stars >= min_stars end)}
+    end)
+    |> Enum.filter(fn c -> Enum.count(c.libraries) > 0 end)
+  end
+
+  defp filter_by_stars(categories, _), do: categories
+
+  def index(conn, params) do
+    categories =
+      AwesomeProvider.categories()
+      |> Enum.map(&transform_category/1)
+      |> filter_by_stars(params["min_stars"])
+
     render(conn, "index.html", %{categories: categories})
   end
 end
