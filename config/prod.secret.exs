be869a84f8fd6804a341abde9f7f29cf88180197
VS <v@s>
2019-12-16 21:48:53 +0300

    Fix config minors

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-import Config
+use Mix.Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -30,7 +30,9 @@ config :app, AppWeb.Endpoint,
     ip: listen_ip,
     port: String.to_integer(System.get_env("PORT") || "4000")
   ],
-  secret_key_base: secret_key_base,
+  secret_key_base: secret_key_base
+
+config :app,
   github_api_token: System.get_env("GITHUB_API_TOKEN")
 
 # ## Using releases (Elixir v1.9+)
