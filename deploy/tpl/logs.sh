6b2a81d2f1bd11e1269fc2c5bcf11bcc885da92b
VS <v@s>
2020-10-08 23:28:45 +0300

    + Deploy scripts

---
+++ deploy/tpl/logs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#
+# This file created automatically by deploy script
+# and will be overwriten on next deploy.
+# All manual changes will be lost.
+# 
+
+docker-compose logs -f app
